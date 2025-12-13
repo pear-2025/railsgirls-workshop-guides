0d67ef3b084ba6899ffcbd913e18682a393aec96
rikuromaru <j2229@matsue-ct.ac.jp>
2025-12-13 04:27:54 +0000

    Calendar: uniform cell sizes and weekend column colors; add calendar.css and update about view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def about
+    # prepare calendar data: month param optional (format: YYYY-MM-DD or YYYY-MM)
+    @month = params[:month].present? ? Date.parse(params[:month]) : Date.current
+
+    if user_signed_in?
+      ideas = current_user.ideas.where.not(date: nil)
+      @ideas_by_date = ideas.group_by { |i| i.date }
+    else
+      @ideas_by_date = {}
+    end
   end
 
   def homepage
